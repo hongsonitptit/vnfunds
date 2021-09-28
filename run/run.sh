aa719859fadf520fb35a2bbd11cb5da612402812
mario <anserteampro@gmail.com>
2021-09-28 16:58:18 +0700

    Update driver for chrome 94

--- run/run.sh
+++ run/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+echo "Run $0"
 CUR_DIR=`dirname $0`
 cd $CUR_DIR/../src
-source activate dev
+#source activate dev
 # craw data
-python crawler.py
+#which python
+/home/mario/miniconda3/envs/dev/bin/python crawler.py
 # push to github
 cd ~/git/vnfunds_data/data
 git add .
